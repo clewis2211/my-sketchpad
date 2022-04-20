ee71ca90a5edd8ce577a2cbcf2c9ff3120d9f9ed
clewis2211 <carltonlewis42@gmail.com>
2022-04-20 13:30:14 -0500

    added function to change the size of the grid

--- scripts.js
+++ scripts.js
@@ -1,24 +1,43 @@
 
 //Sketchpad
 let sketchPad = document.getElementById('sketchPad');
-sketchPad.addEventListener('onmousedown',createSquare)
+sketchPad.style.gridTemplateColumns = 'repeat(16, 1fr)'
+sketchPad.style.gridTemplateRows = 'repeat(16, 1fr)'
+sketchPad.addEventListener('mousedown',createSquare)
+// sketchPad.addEventListener('dblclick', )
 // sketchPad.addEventListener('mousemove', createSquare)
 
-//Clear Button
+// Clear Button
 let clearBtn = document.getElementById('clearBtn')
 clearBtn.disabled = false
 clearBtn.addEventListener('click', clearSketchPad)
 
-
+//Change Grid Size Button
+let gridSizeBtn = document.getElementById('gridSizeBtn')
+gridSizeBtn.addEventListener('click', changeSize)
 
 function createSquare(){
-    let square = document.createElement('div');
-    square.className = 'squareDiv'
-    square.addEventListener('mousedown', function(){
-        square.style.backgroundColor = changeColor()
-    })
-    sketchPad.appendChild(square)
+    for(let i = 0; i < 256; i++){
+        let square = document.createElement('div')
+        square.style.backgroundColor = 'white';
+        sketchPad.insertAdjacentElement('beforeend', square)
+        square.className = 'squareDiv'
+        square.addEventListener('mouseover', function(){
+            square.style.backgroundColor = changeColor()
+        })
+        // square.addEventListener('dblclick', function(){
+        //     square.innerText = exit()
+        // })
+    }
+    // sketchPad.appendChild(square)
+
+    
 }
+
+function exit(e){
+    square.innerText = ''
+}
+
 
 function changeColor() {
     var letters = '0123456789ABCDEF';
@@ -33,4 +52,24 @@ function clearSketchPad(e){
     console.log(e.target.id)
     let clearSketch = document.getElementById('sketchPad')
     clearSketch.innerText = ''
+}
+
+function changeSize(e){
+   e.preventDefault()
+
+    let changeGridSize = document.getElementById('changeGridSize').value;
+    console.log(changeGridSize)
+    // sketchPad.style.gridTemplateColumns = 'repeat(16, 1fr)'
+    // sketchPad.style.gridTemplateRows = 'repeat(16, 1fr)'
+
+    sketchPad.style.gridTemplateColumns = `repeat(${changeGridSize}, 1fr)`
+    sketchPad.style.gridTemplateRows = `repeat(${changeGridSize}, 1fr)`
+
+    for(let i = 0; i < changeGridSize.length; i++){
+        let square = document.createElement('div')
+        square.style.backgroundColor = 'white';
+        sketchPad.insertAdjacentElement('beforeend', square)
+        square.className = 'squareDiv'
+    }
+
 }
